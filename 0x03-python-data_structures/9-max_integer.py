5b50a6c126d6b1f9bc27d4908aade36a3f0f715b
eleab cloud <eleniababu@gmail.com>
2023-06-12 05:33:53 -0700

    max in a list

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if my_list:
-        return (min(my_list, key=lambda i: -i))
-    else None
+    return (min(my_list, key=lambda i: -i)) if my_list else None
